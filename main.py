599b3468cb41c9c655b6e47719945cdedf4f41fd
BB-Simon <simon.chowdery@gmail.com>
2026-04-21 16:37:21 +0200

    Add product crud API endpoints

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from models import Product
 from database import session, engine
 
 import database_models
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
@@ -18,23 +19,67 @@ products = [
   Product(id=2, name='Productc #2', description='Product #2 description', price=2, quantity=20),
 ]
 
+def get_db():
+  db = session()
+  try:
+    yield db
+  finally:
+    db.close()
+
+
+def init_db():
+  db = session()
+  count = db.query(database_models.Product).count
+  if(count == 0):
+    for product in products:
+      db.add(database_models.Product(**product.model_dump()))
+  
+  db.commit()
+
+
+init_db()
+
 @app.get('/api/products')
-def getProducts():
-  db = session
-  return products
+def getProducts(db: Session = Depends(get_db)):
+  db_products = db.query(database_models.Product).all()
+  return db_products
 
 @app.get('/api/product/{id}')
-def get_product(id: int):
-  for product in products:
-    if product.id == id:
-      return product
+def get_product(id: int, db: Session = Depends(get_db)):
+  db_product = db.query(database_models.Product).filter(database_models.Product.id == id).first()
+  if db_product:
+    return db_product
     
-    return 'Product not found'
+  return 'Product not found'
   
 
 @app.post('/api/product')
-def add_product():
-  products.append(
-      Product(id=3, name='Productc #3',
-              description='Product #3 description', price=2, quantity=20)
-  )
+def add_product(product: Product, db: Session = Depends(get_db)):
+  db.add(database_models.Product(**product.model_dump()))
+  db.commit()
+  return product
+
+@app.put('/api/product')
+def add_product(id: int, product: Product, db: Session = Depends(get_db)):
+  db_product = db.query(database_models.Product).filter(
+      database_models.Product.id == id).first()
+  if db_product:
+    db_product.name = product.name
+    db_product.description = product.description
+    db_product.price = product.price
+    db_product.quantity = product.quantity
+    db.commit()
+    return 'Product updated successfully'
+
+  return 'Product not found'
+
+@app.delete('/api/product')
+def add_product(id: int, db: Session = Depends(get_db)):
+  db_product = db.query(database_models.Product).filter(
+      database_models.Product.id == id).first()
+  if db_product:
+    db.delete(db_product)
+    db.commit()
+    return 'Product deleted successfully'
+
+  return 'Product not found'
